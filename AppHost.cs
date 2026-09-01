964504f7436b0551c50d5f830a7f581febb8c26f
David Osolkowski <david.osolkowski@vynedental.com>
2026-09-01 15:31:56 -0400

    Clean up configuration

--- AppHost.cs
+++ AppHost.cs
@@ -16,9 +16,7 @@ builder
     .AddContainer(tykService, "tykio/tyk-gateway", "v5.14.0")
     .WithCertificateTrustConfiguration(context =>
     {
-        //context.EnvironmentVariables["TYK_GW_OPENTELEMETRY_TLS_CAFILE"] = context.CertificateBundlePath;
         context.EnvironmentVariables["TYK_GW_OPENTELEMETRY_TRACES_TLS_CAFILE"] = context.CertificateBundlePath;
-        //context.EnvironmentVariables["TYK_GW_OPENTELEMETRY_METRICS_TLS_CAFILE"] = context.CertificateBundlePath;
 
         return Task.CompletedTask;
     })
@@ -27,39 +25,24 @@ builder
     {
         if (context.EnvironmentVariables["OTEL_EXPORTER_OTLP_ENDPOINT"] is EndpointReference endpoint)
         {
-            // Note that just hard-coding this to "localhost:21024" (matching ASPIRE_DASHBOARD_OTLP_ENDPOINT_URL in launchSettings)
-            // does not work either.
             var reference = ReferenceExpression.Create($"{endpoint.Property(EndpointProperty.HostAndPort)}");
-            //context.EnvironmentVariables["TYK_GW_OPENTELEMETRY_ENDPOINT"] = reference;
             context.EnvironmentVariables["TYK_GW_OPENTELEMETRY_TRACES_ENDPOINT"] = reference;
-            //context.EnvironmentVariables["TYK_GW_OPENTELEMETRY_METRICS_ENDPOINT"] = reference;
         }
 
         if (context.EnvironmentVariables["OTEL_EXPORTER_OTLP_HEADERS"] is string headers)
         {
-            // This shouldn't be here because we set Dashboard:Otlp:AuthMode to Unsecured, but it is anyway, and even
-            // though it is here, we got 403 errors from trying to send telemetry via http.
             string tykHeaders = headers.Replace("=", ":", StringComparison.Ordinal);
-            //context.EnvironmentVariables["TYK_GW_OPENTELEMETRY_HEADERS"] = tykHeaders;
             context.EnvironmentVariables["TYK_GW_OPENTELEMETRY_TRACES_HEADERS"] = tykHeaders;
-            //context.EnvironmentVariables["TYK_GW_OPENTELEMETRY_METRICS_HEADERS"] = tykHeaders;
         }
     })
+    // General configuration
+    .WithEnvironment("TYK_GW_LOGFORMAT", "json")
     .WithEnvironment("TYK_GW_LOGLEVEL", "debug")
     .WithEnvironment("TYK_GW_LISTENPORT", "8080")
     .WithEnvironment("TYK_GW_CONTROLAPIPORT", "8081")
-    .WithEnvironment("TYK_GW_LOGFORMAT", "json")
+    .WithEnvironment("TYK_GW_SECRET", tykPassword)
+    // Access logs just write to the console
     .WithEnvironment("TYK_GW_ACCESSLOGS_ENABLED", "true")
-    // General OTel
-    /*
-    .WithEnvironment("TYK_GW_OPENTELEMETRY_ENABLED", "true")
-    .WithEnvironment("TYK_GW_OPENTELEMETRY_EXPORTER", tykOtelFormat)
-    .WithEnvironment("TYK_GW_OPENTELEMETRY_CONNECTIONTIMEOUT", "10")
-    .WithEnvironment("TYK_GW_OPENTELEMETRY_RESOURCENAME", tykService)
-    .WithEnvironment("TYK_GW_OPENTELEMETRY_TLS_ENABLE", tykOtelUseTls)
-    .WithEnvironment("TYK_GW_OPENTELEMETRY_CONTEXTPROPAGATION", "tracecontext")
-    .WithEnvironment("TYK_GW_OPENTELEMETRY_SAMPLING_TYPE", "AlwaysOn")
-    */
     // OTel Traces
     .WithEnvironment("TYK_GW_OPENTELEMETRY_TRACES_ENABLED", "true")
     .WithEnvironment("TYK_GW_OPENTELEMETRY_TRACES_EXPORTER", tykOtelFormat)
@@ -70,23 +53,19 @@ builder
     .WithEnvironment("TYK_GW_OPENTELEMETRY_TRACES_SAMPLING_TYPE", "AlwaysOn")
     // OTel Metrics
     .WithEnvironment("TYK_GW_OPENTELEMETRY_METRICS_ENABLED", "true")
-    //.WithEnvironment("TYK_GW_OPENTELEMETRY_METRICS_EXPORTER", tykOtelFormat)
-    //.WithEnvironment("TYK_GW_OPENTELEMETRY_METRICS_CONNECTIONTIMEOUT", "10")
-    //.WithEnvironment("TYK_GW_OPENTELEMETRY_METRICS_RESOURCENAME", tykService)
-    //.WithEnvironment("TYK_GW_OPENTELEMETRY_METRICS_TLS_ENABLE", tykOtelUseTls)
     .WithEnvironment("TYK_GW_OPENTELEMETRY_METRICS_EXPORTINTERVAL", "15")
-    .WithEnvironment("TYK_GW_SECRET", tykPassword)
+    // Redis
     .WithEnvironment("TYK_GW_STORAGE_USESSL", "true")
     .WithEnvironment("TYK_GW_STORAGE_HOST", redis.Resource.Host)
     .WithEnvironment("TYK_GW_STORAGE_PORT", redis.Resource.Port)
     .WithEnvironment("TYK_GW_STORAGE_PASSWORD", redisPassword)
+    // Docker
     .WithBindMount("TykApps", "/opt/tyk-gateway/apps")
+    // Aspire
     .WithHttpEndpoint(8080, 8080, "data")
     .WithHttpEndpoint(8081, 8081, "control")
     .WithExternalHttpEndpoints()
     .WithReference(redis)
     .WaitFor(redis);
-
-//.WaitFor(otelCollector);
 
 await builder.Build().RunAsync().ConfigureAwait(false);
